30f538c4e169a67c23993ce483dec95f8014364e
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-07-14 16:40:17 -0400

    updates

--- src/components/User.js
+++ src/components/User.js
@@ -1,26 +1,13 @@
 function User({ user }) {
   const { name, cell, dob, email, picture } = user;
   const fullName = `${name.first} ${name.last}`;
-  const picSrc = picture.medium;
-
-  // const userData = () => (
-  //   <div className="user">
-  //     <img src={picSrc} alt={`${fullName} picture`} />
-  //     <h2>{fullName}</h2>
-  //     <div className="user-info">
-  //       <div className="user-dob">{dob}</div>
-  //       <div className="user-cell">{cell}</div>
-  //       <div className="user-email">{email}</div>
-  //     </div>
-  //   </div>
-  // );
+  const picSrc = picture.large;
 
   return (
     <div className="user">
       <img src={picSrc} alt={`${fullName} picture`} />
       <h2>{fullName}</h2>
       <div className="user-info">
-        <div className="user-dob">{dob}</div>
         <div className="user-cell">{cell}</div>
         <div className="user-email">{email}</div>
       </div>
--- src/services/rando_user.js
+++ src/services/rando_user.js
@@ -3,6 +3,11 @@ import axios from "axios";
 export default async function fetchUser() {
   const BASE_URL = "https://api.randomuser.me/";
 
-  const response = await axios.get(BASE_URL);
-  return response.data.results[0];
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data.results[0];
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
